1dd7f2b1ac193ba907723ec8b2c539e6f5d9acb2
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-05-13 21:37:41 -0400

    final commit, could not get compareTo() to not return tuples in some cases

--- recursion.py
+++ recursion.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module creates recursive functions."""
+
+
 def fibonacci(n):
     if n<0:
         print("Input must be a positive number!")
@@ -9,46 +14,31 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 
-#print fibonacci(10)
-
-
 def gcd(a, b):
     if a == 0:
         return b
     return gcd(b % a, a)
 
 
-# #print gcd(30, 20)
-#
-# def compareTo(s1, s2):
-#     if s1[0] < s2[0]:
-#         return -1,compareTo(s1[1:], s2[1:])
-#     elif s1[0] == s2[0]:
-#         return 0,compareTo(s1[1:], s2[1:])
-#     elif s1[0] > s2[0]:
-#         return +1,compareTo(s1[1:], s2[1:])
-#     #compareTo(s1[1:], s2[1:])
-#
-# print compareTo('Happy', 'Happer')
-
 
 def compareTo(s1, s2):
+    #Hi Professor,
+    # i could not figure out how to get my results with out it returning tuples
+    #in time. i hope i came close.
+    # if possable, can you send me the correct code to make this function work?
     if len(s1) < len(s2):
         return -1
     elif len(s1) > len(s2):
         return +1
     else:
         if len(s1) == len(s2):
-            # result= 0
             for item in s1:
                 if s1[0] < s2[0]:
-                    return -1 # , compareTo(s1[1:], s2[1:])
+                    return -1
                 elif s1[0] > s2[0]:
-                    return +1 # , compareTo(s1[1:], s2[1:])
+                    return +1
                 elif s1[0] == s2[0]:
-                    # maybe an if statement here?
-                    # compareTo(s1[1:], s2[1:])
                     return 0, compareTo(s1[1:], s2[1:])
 
 
-print compareTo('apples', 'applet')
+
